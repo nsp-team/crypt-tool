e7de42b11009f0e14b653d56a1387c98ec3ad973
maoxp <maoxingpei8686@163.com>
2021-11-10 15:16:03 +0800

    init

---
+++ src/Crypt.php
@@ -0,0 +1,96 @@
+<?php
+
+//declare(strict_types=1);
+namespace NspTeam\Crypt;
+
+use Exception;
+use LengthException;
+use phpseclib3\Crypt\AES;
+use phpseclib3\Exception\BadDecryptionException;
+
+/**
+ * This class encrypts and decrypts plain text using AES-128 with a variable length initialization vector. IV is
+ * generated using openssl pseudo random.
+ */
+class Crypt
+{
+    public const DEFAULT_IV_LENGTH = 16;
+
+    /**
+     * A CBC-mode AES object using the RSA PKCS padding standards for padding.
+     * @var AES
+     */
+    protected $aesCrypt;
+
+    public function __construct(string $secretKey)
+    {
+        $aesCrypt = new AES('cbc');
+        $aesCrypt->setKey($secretKey);
+        $this->setAesCrypt($aesCrypt);
+    }
+
+    /**
+     * @param int $ivLength should match AES block size which is 16/128 bytes/bits
+     * @throws Exception
+     */
+    public function encrypt(string $plainText, int $ivLength = self::DEFAULT_IV_LENGTH): string
+    {
+        $iv       = $this->generateIV($ivLength);
+        $aesCrypt = $this->getAesCrypt();
+        $aesCrypt->setIV($iv);
+
+        return base64_encode($aesCrypt->encrypt($plainText) . $iv);
+    }
+
+    /**
+     * @return string when bad data is passed in, the return value will be an empty string
+     */
+    public function decrypt(string $encodedData, int $ivLength = self::DEFAULT_IV_LENGTH): string
+    {
+        $data       = base64_decode($encodedData);
+        $iv         = substr($data, -$ivLength);
+        $cipherText = substr($data, 0, -$ivLength);
+        $aesCrypt   = $this->getAesCrypt();
+
+        try {
+            $aesCrypt->setIV($iv);
+        } catch (LengthException $e) {
+            return '';
+        }
+
+        try {
+            return $aesCrypt->decrypt($cipherText);
+        } catch (BadDecryptionException|LengthException $e) {
+            return '';
+        }
+    }
+
+    protected function setAesCrypt(AES $aesCrypt): self
+    {
+        $this->aesCrypt = $aesCrypt;
+        return $this;
+    }
+
+    protected function getAesCrypt(): AES
+    {
+        return $this->aesCrypt;
+    }
+
+    /**
+     * Generate initialization vector of specified length in bytes.
+     *
+     * @throws Exception
+     * @see https://stackoverflow.com/questions/7280769/how-to-securely-generate-an-iv-for-aes-cbc-encryption
+     */
+    private function generateIV(int $length): string
+    {
+        if ($length < 1) {
+            throw new Exception('The length of the desired string of bytes must be a positive integer.');
+        }
+        $iv = openssl_random_pseudo_bytes($length, $wasItSecure);
+        if (!$wasItSecure) {
+            throw new Exception('Non-cryptographically strong algorithm used for iv generation. This IV is not safe to use.');
+        }
+        return $iv;
+    }
+}
---
+++ tests/CryptTest.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * Copyright 2021 Glu Mobile Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace NspTeam\Tests;
+
+use NspTeam\Crypt\Crypt;
+use Exception;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Tests for class \NspTeam\Crypt\Crypt.
+ *
+ * @internal
+ * @coversNothing
+ */
+class CryptTest extends TestCase
+{
+    protected const TEST_DATA = 'test_message';
+
+    protected const TEST_KEY1 = '1234567890123456';
+
+    protected const TEST_KEY2 = '9876543210123456';
+
+    public function dataEncryption(): array
+    {
+        return [
+            [
+                44,
+                self::TEST_DATA,
+                self::TEST_KEY1,
+                'Encrypt a string with the first key.',
+            ],
+            [
+                44,
+                self::TEST_DATA,
+                self::TEST_KEY2,
+                'Encrypt a string with the second key.',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataEncryption
+     * @covers \NspTeam\Crypt\Crypt::encrypt
+     */
+    public function testEncryption(int $expectedLength, string $data, string $key, string $message)
+    {
+        $encryptedData = (new Crypt($key))->encrypt($data);
+
+        $cipherText = substr(base64_decode($encryptedData), 0, -Crypt::DEFAULT_IV_LENGTH);
+        // does not measure security but ensures the cipher text is some distance away from the original message
+        self::assertGreaterThan(5, levenshtein($data, $cipherText));
+
+        self::assertSame($expectedLength, strlen($encryptedData), $message);
+    }
+
+    public function dataDecryption(): array
+    {
+        return [
+            [
+                '',
+                '',
+                self::TEST_KEY1,
+                'Decrypt an empty string.',
+            ],
+            [
+                '',
+                'invalid-key',
+                self::TEST_KEY1,
+                'Decrypt a invalid base64-encoded string.',
+            ],
+            [
+                '',
+                'YQ==',
+                self::TEST_KEY1,
+                'Decrypt a short string (a bad encrypted string; base64-encoded).',
+            ],
+            [
+                '',
+                'OGs3emdkaTlqd3ltMmh6Y21ubjJqMmp5Y25sMDU1ZHhsOWNod2poZWU3MG8=',
+                self::TEST_KEY1,
+                'Decrypt a long string "8k7zgdi9jwym2hzcmnn2j2jycnl055dxl9chwjhee70o" (a bad encrypted string; base64-encoded).',
+            ],
+
+            [
+                self::TEST_DATA,
+                'Btpxr9R00y2/69lseobzCPCv95ru0yvbN2tzGZphmqs=',
+                self::TEST_KEY1,
+                'Decrypt a string with the first key (the correct one).',
+            ],
+            [
+                self::TEST_DATA,
+                'LbdYEJg0GjBl7aVqhFu+QpwabowYX87qy/9itTu8nOE=',
+                self::TEST_KEY1,
+                'Decrypt another string with the first key (the correct one).',
+            ],
+            [
+                '',
+                'Btpxr9R00y2/69lseobzCPCv95ru0yvbN2tzGZphmqs=',
+                self::TEST_KEY2,
+                'Decrypt a string with the second key (the wrong one).',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataDecryption
+     * @covers \NspTeam\Crypt\Crypt::decrypt
+     */
+    public function testDecryption(string $expectedData, string $encryptedData, string $key, string $message)
+    {
+        self::assertSame($expectedData, (new Crypt($key))->decrypt($encryptedData), $message);
+    }
+
+    /**
+     * @covers \NspTeam\Crypt\Crypt::generateIV
+     */
+    public function testBadIVLength()
+    {
+        self::expectException(Exception::class);
+        self::expectExceptionMessage('The length of the desired string of bytes must be a positive integer.');
+
+        (new Crypt(static::TEST_KEY1))->encrypt('test_message', 0);
+    }
+}
